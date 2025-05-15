8e9498438f73b7e44556f859e322271bfbd1be74
nihal9075 <nihal9075.su@gmail.com>
2025-05-15 10:50:45 +0600

    Arithmetic Operators

---
+++ Arithmetic_Operators.py
@@ -0,0 +1,12 @@
+
+a = int(input())
+b = int(input())
+
+# Arithmetic Operators
+sum = a + b
+difference = a - b
+product = a * b
+# Print the results 
+print(sum)
+print(difference)   
+print(product)
